72e2610e4a2fdb9c2e651ffee5392266357482a4
marvidik <ebubeidika@gmail.com>
2025-06-09 00:34:47 +0100

    static files migrstion

--- Realestatebackend/settings.py
+++ Realestatebackend/settings.py
@@ -121,7 +121,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+
+# Where to collect static files (absolute path)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# (Optional) If you have additional static folders:
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
