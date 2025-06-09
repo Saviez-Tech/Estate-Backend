73f0f62cf12a0788fcaf3a7f765fd4f7884fc210
marvidik <ebubeidika@gmail.com>
2025-06-09 12:34:47 +0100

    fixed the image display issue

--- Realestatebackend/urls.py
+++ Realestatebackend/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from listings.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +31,8 @@ urlpatterns = [
     path('contact/', contact_message_create, name='contact-message-create'),
     path('newsletter/subscribe/', subscribe_newsletter, name='subscribe-newsletter'),
 ]
+
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
